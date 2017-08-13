bb4330e19bf284762bba45060cb0f78e6064120d
Pedro Resende <pedroresende@mail.resende.biz>
2017-08-13 20:03:23 +0100

    Added Symfony\Bundle\WebServerBundle\WebServerBundle to the Kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Nelmio\ApiDocBundle\NelmioApiDocBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
 
         return $bundles;
